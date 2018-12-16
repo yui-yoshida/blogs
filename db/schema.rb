f5df5dbc2f41bb3e880880b97af84648c979ab47
github yui-yoshida <yui.0618.kj8@gmail.com>
2018-12-16 12:51:50 +0900

    userテーブルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_16_024832) do
+ActiveRecord::Schema.define(version: 2018_12_16_034938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2018_12_16_024832) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
